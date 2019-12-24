8350dbda7b441cb150ae16c1461b789b9a8ce5f9
aavillalbab <arithalexis@gmail.com>
2019-12-24 14:18:57 -0500

    field thirdPartyId is added

--- src/third-party-accounts/dto/third-party-account.dto.ts
+++ src/third-party-accounts/dto/third-party-account.dto.ts
@@ -2,6 +2,9 @@ import { ApiProperty } from '@nestjs/swagger';
 
 export class ThirdPartyAccountDto {
     @ApiProperty()
+    thirdPartyId: number;
+
+    @ApiProperty()
     bankId: number;
 
     @ApiProperty()
