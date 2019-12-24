75af4cd0453e808941667d3ea5b5a4c2c4bb63f1
aavillalbab <arithalexis@gmail.com>
2019-12-23 19:02:47 -0500

    module regime finished

--- src/regime/regime.controller.ts
+++ src/regime/regime.controller.ts
@@ -1,4 +1,57 @@
-import { Controller } from '@nestjs/common';
+import {
+    Controller,
+    Post,
+    Res,
+    Body,
+    HttpStatus,
+    Param,
+    Put,
+    Get,
+    Query,
+    Patch,
+    UseGuards
+} from '@nestjs/common';
 
-@Controller('regime')
-export class RegimeController {}
+import { Response } from 'express';
+
+import {
+    ApiTags,
+    ApiConsumes,
+    ApiOperation,
+    ApiBearerAuth
+} from '@nestjs/swagger';
+
+import { AuthGuard } from 'sigasac-utils';
+
+import { RegimeService } from './regime.service';
+
+@Controller('sigasac/v1/regimes')
+@ApiBearerAuth()
+@ApiTags('rigimes')
+export class RegimeController {
+    constructor(private readonly regimeService: RegimeService) {}
+
+    @Get()
+    @ApiOperation({})
+    @UseGuards(AuthGuard('jwt'))
+    async getAll(@Res() res: Response) {
+        try {
+            const regimes = await this.regimeService.getAll();
+
+            res.status(HttpStatus.OK).send({
+                regimes
+            });
+        } catch (error) {
+            if (error.message.statusCode) {
+                return res.status(error.message.statusCode).send({
+                    message: error.message
+                });
+            }
+
+            res.status(HttpStatus.INTERNAL_SERVER_ERROR).send({
+                message: error.message,
+                stack: error.stack
+            });
+        }
+    }
+}
--- src/regime/regime.service.ts
+++ src/regime/regime.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { throws } from 'assert';
+import { DatabaseProvider, Regime } from 'sigasac-db';
 
 @Injectable()
-export class RegimeService {}
+export class RegimeService {
+    async getAll() {
+        try {
+            const connection = await DatabaseProvider.getConnection();
+
+            const regimes = await connection.getRepository(Regime).find();
+
+            return regimes;
+        } catch (error) {
+            throw error;
+        }
+    }
+}
