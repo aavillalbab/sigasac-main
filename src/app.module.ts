fcfdad0dcf894cd8f42c6dc0e49a76235f928ee3
aavillalbab <arithalexis@gmail.com>
2020-01-08 17:49:36 -0500

    order imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,31 +5,33 @@ import { AuthModule } from 'sigasac-utils';
 import { AccountsTypesModule } from './accounts-types/accounts-types.module';
 import { BankModule } from './bank/bank.module';
 import { CampusModule } from './campus/campus.module';
+import { BudgetAccountsModule } from './budget-accounts/budget-accounts.module';
+import { BudgetsModule } from './budgets/budgets.module';
 import { CountryModule } from './country/country.module';
 import { LoginModule } from './login/login.module';
 import { NatureDocumentsModule } from './nature-documents/nature-documents.module';
+import { PeopleTypeModule } from './people-type/people-type.module';
+import { ProjectsModule } from './projects/projects.module';
+import { RevenueModule } from './revenue/revenue.module';
+import { RevenueTypeModule } from './revenue-type/revenue-type.module';
+import { RegimeModule } from './regime/regime.module';
 import { SchoolModule } from './school/school.module';
 import { SchoolBankAccountsModule } from './school-bank-accounts/school-bank-accounts.module';
 import { SingleAccountPlanModule } from './single-account-plan/single-account-plan.module';
-import { PeopleTypeModule } from './people-type/people-type.module';
-import { ProjectsModule } from './projects/projects.module';
-import { RegimeModule } from './regime/regime.module';
-import { RevenueTypeModule } from './revenue-type/revenue-type.module';
-import { RevenueModule } from './revenue/revenue.module';
-import { ThirdPartyTypesModule } from './third-party-types/third-party-types.module';
 import { ThirdPartyModule } from './third-party/third-party.module';
-import { VoucherModule } from './voucher/voucher.module';
 import { ThirdPartyAccountsModule } from './third-party-accounts/third-party-accounts.module';
-import { BudgetAccountsModule } from './budget-accounts/budget-accounts.module';
+import { ThirdPartyTypesModule } from './third-party-types/third-party-types.module';
 import { TypesAdministratorDocumentsModule } from './types-administrator-documents/types-administrator-documents.module';
 import { TypesSchoolDocumentsModule } from './types-school-documents/types-school-documents.module';
-import { BudgetsModule } from './budgets/budgets.module';
+import { VoucherModule } from './voucher/voucher.module';
 
 @Module({
     imports: [
         AuthModule,
         AccountsTypesModule,
         BankModule,
+        BudgetAccountsModule,
+        BudgetsModule,
         CampusModule,
         CountryModule,
         LoginModule,
@@ -37,19 +39,17 @@ import { BudgetsModule } from './budgets/budgets.module';
         PeopleTypeModule,
         ProjectsModule,
         RegimeModule,
-        RevenueTypeModule,
         RevenueModule,
+        RevenueTypeModule,
         SchoolModule,
         SchoolBankAccountsModule,
         SingleAccountPlanModule,
         ThirdPartyModule,
-        ThirdPartyTypesModule,
-        VoucherModule,
         ThirdPartyAccountsModule,
-        BudgetAccountsModule,
+        ThirdPartyTypesModule,
         TypesAdministratorDocumentsModule,
         TypesSchoolDocumentsModule,
-        BudgetsModule
+        VoucherModule
     ]
 })
 export class AppModule {}
